345bc3e0ee38c949a40bb6d5c21703ab934a0894
CompulsiveCoder <compulsivecoder@gmail.com>
2015-08-03 07:34:35 +0000

    senduino.py auto detects port

--- py/senduino.py
+++ py/senduino.py
@@ -1,19 +1,50 @@
 import serial
 import time
 import sys
+import fnmatch
 
-print "Opening serial port"
-print ""
 
-arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1);
-time.sleep(2);
+def auto_detect_serial_unix(preferred_list=['*']):
+    '''try to auto-detect serial ports on win32'''
+    import glob
+    glist = glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
+    ret = []
 
-for arg in sys.argv:
-    command = arg
+    # try preferred ones first
+    for d in glist:
+        for preferred in preferred_list:
+            if fnmatch.fnmatch(d, preferred):
+                ret.append(d)
+    if len(ret) > 0:
+        return ret
+    # now the rest
+    for d in glist:
+        ret.append(d)
+    return ret
 
-arduino.write(command);
+def main():
 
-print (arduino.read(100))
+    print "Opening serial port"
+    print ""
 
-print ""
-print "Finished"
+    available_ports = auto_detect_serial_unix()
+    port = serial.Serial(available_ports[0], 9600,timeout=1)
+    print port.name
+
+
+    arduino = serial.Serial(port.name, 9600, timeout = 1);
+    time.sleep(2);
+
+    for arg in sys.argv:
+        command = arg
+
+    arduino.write(command);
+
+    print (arduino.read(100))
+
+    print ""
+    print "Finished"
+
+    return 0
+
+main()
