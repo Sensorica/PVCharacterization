6345e94ec2586ea78b1f773f0c8e81c737072c17
snowy13 <jim.anastassiou@gmail.com>
2016-04-05 20:18:23 -0400

    Getting there

--- pv3.py
+++ pv3.py
@@ -1,5 +1,6 @@
-from gi.repository import Gtk as gtk, Gdk as gdk
+#!/usr/bin/python3
 
+from gi.repository import Gtk as gtk, Gdk as gdk
 import time
 import serial
 
@@ -22,9 +23,24 @@ class PV:
         self.aboutdialog.hide()
 
     def on_goto_button_clicked(self, button, data=None):
-        self.x_entry = self.builder.get_object("x_goto")
-        self.y_entry = self.builder.get_object("y_goto")
-
+        self.x_entry = self.builder.get_object("x_entry")
+        self.x = (self.x_entry.get_text())
+        xGoTo = (self.x)
+        xGoTo = float(xGoTo)
+        self.y_entry = self.builder.get_object("y_entry")
+        self.y = (self.y_entry.get_text())
+        yGoTo = (self.y)
+        yGoTo = float(yGoTo)
+        xGoTo = xGoTo*100
+        yGoTo = yGoTo*100
+        xGoTo = int(xGoTo)
+        yGoTo = int(yGoTo)
+        for x in range(0, xGoTo, 10):
+            print("par angleOffsetPitch %d" % x)
+            time.sleep(0.5) 
+ #  print("par angleOffsetRoll %d" % yGoTo)
+        self.response =  self.warning.run()
+        self.warning.hide()
     def on_move_button_clicked(self, button, data=None):
         self.x_entry = self.builder.get_object("x_move")
         self.y_entry = self.builder.get_object("y_move")
@@ -33,14 +49,11 @@ class PV:
         self.x_entry = self.builder.get_object("x_move")
         self.y_entry = self.builder.get_object("y_move")
 
-
     def on_run_button_clicked(self, button, data=None):
         self.x_entry = self.builder.get_object("x_move")
         self.y_entry = self.builder.get_object("y_move")
 
-
     def __init__(self):
-        serial.Serial('/dev/ttyACM0', 115200, timeout=1)
         cssProvider = gtk.CssProvider()
         cssProvider.load_from_path('pv.css')
         screen = gdk.Screen.get_default()
@@ -52,8 +65,13 @@ class PV:
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("window1")
         self.aboutdialog = self.builder.get_object("aboutdialog1")
+        self.warning = self.builder.get_object("messagedialog1")
         self.window.show()
+#        serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+
 
 if __name__ == "__main__":
     main = PV()
     gtk.main()
+
+
